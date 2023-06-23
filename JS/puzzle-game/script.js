7bec00863d4357138f0c77b323f3a4bb1cf9ef3b
Mohamed Khaled Samir <mouhamed_khaled@yahoo.com>
2023-06-23 15:34:03 +0300

    update project name and adding comments

--- JS/puzzle-game/script.js
+++ JS/puzzle-game/script.js
@@ -2,7 +2,7 @@ const btnStart = document.querySelector(".btn-start");
 const container = document.querySelector(".container");
 const puzzle = document.querySelector(".puzzle");
 const pieces = document.querySelectorAll(".piece");
-const movesSpan = document.querySelector(".moves");
+const movesCountSpan = document.querySelector(".moves");
 const winScreen = document.querySelector(".win-screen");
 const btnAgain = document.querySelector(".btn-again");
 const result = document.querySelector(".result");
@@ -18,10 +18,11 @@ btnAgain.addEventListener("click", () => {
 
 movingPiecesLogic();
 
-//functions
+//FUNCTIONS//
 function movingPiecesLogic() {
-  pieces.forEach((piece, index) => {
+  pieces.forEach((piece) => {
     piece.addEventListener("click", () => {
+      //getting the place coordinates of the clicked piece
       let pieceStyle = getComputedStyle(piece);
       let colStart = parseInt(pieceStyle.getPropertyValue("grid-column-start"));
       let colEnd = parseInt(pieceStyle.getPropertyValue("grid-column-end"));
@@ -30,24 +31,28 @@ function movingPiecesLogic() {
 
       let aroundPieces = [
         {
+          //right piece
           colStart: colStart + 1,
           colEnd: colEnd + 1,
           rowStart: rowStart,
           rowEnd: rowEnd,
         },
         {
+          //left piece
           colStart: colStart - 1,
           colEnd: colEnd - 1,
           rowStart: rowStart,
           rowEnd: rowEnd,
         },
         {
+          //above piece
           colStart: colStart,
           colEnd: colEnd,
           rowStart: rowStart - 1,
           rowEnd: rowEnd - 1,
         },
         {
+          //below piece
           colStart: colStart,
           colEnd: colEnd,
           rowStart: rowStart + 1,
@@ -55,19 +60,18 @@ function movingPiecesLogic() {
         },
       ];
 
-      aroundPieces.forEach((aroundPiece, index) => {
-        let aroundPieceDiv = document.querySelector(
-          `.col${aroundPiece.colStart}-${aroundPiece.colEnd}.row${aroundPiece.rowStart}-${aroundPiece.rowEnd}`
-        );
-
+      //find the empty piece and switch with it
+      aroundPieces.forEach((aroundPiece) => {
+        let aroundPieceSelector = `.col${aroundPiece.colStart}-${aroundPiece.colEnd}.row${aroundPiece.rowStart}-${aroundPiece.rowEnd}`;
+        let aroundPieceDiv = document.querySelector(aroundPieceSelector);
+        //check if the selector exists
         if (!aroundPieceDiv) return;
-
         if (aroundPieceDiv.classList.contains(`empty-piece`)) {
           //switching places
           aroundPieceDiv.className = `piece empty-piece col${pieceStyle.gridColumnStart}-${pieceStyle.gridColumnEnd} row${pieceStyle.gridRowStart}-${pieceStyle.gridRowEnd}`;
           piece.className = `piece col${aroundPiece.colStart}-${aroundPiece.colEnd} row${aroundPiece.rowStart}-${aroundPiece.rowEnd}`;
           moves++;
-          movesSpan.textContent = `${moves} moves`;
+          movesCountSpan.textContent = `${moves} moves`;
         }
       });
 
@@ -76,6 +80,7 @@ function movingPiecesLogic() {
   });
 }
 function checkRightOrder() {
+  //The right order for the parts
   let part1 = document.querySelector("#part1.col1-2.row1-2"),
     part2 = document.querySelector("#part2.col2-3.row1-2"),
     part3 = document.querySelector("#part3.col3-4.row1-2"),
@@ -86,6 +91,7 @@ function checkRightOrder() {
     part8 = document.querySelector("#part8.col2-3.row3-4"),
     part9 = document.querySelector("#part9.col3-4.row3-4");
 
+  //check if each part is in the right place
   if (
     part1 &&
     part2 &&
@@ -97,11 +103,14 @@ function checkRightOrder() {
     part8 &&
     part9
   ) {
-    puzzle.classList.add("win");
-    setTimeout(() => {
-      winScreen.classList.add("win");
-    }, 600);
-
-    result.textContent = `You did it with ${moves} moves`;
+    win();
   }
+}
+function win() {
+  puzzle.classList.add("win");
+  setTimeout(() => {
+    document.body.classList.add("win");
+  }, 600);
+
+  result.textContent = `You did it with ${moves} moves`;
 }
